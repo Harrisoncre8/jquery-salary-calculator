49c81cbfbeb56bef9b1f6c0e0544a352e4c48a4e
Harrison Nguyen <nguy2173@umn.edu>
2019-11-08 15:45:44 -0600

    Created addEmployee function that gets the values of the inputs from DOM

--- client.js
+++ client.js
@@ -4,3 +4,34 @@ function onReady(params) {
     console.log('Hi from jQuery');
 }
 
+// store employee information here
+let employeeArray = [];
+
+// take in information and push to employeeArray
+function addEmployee () {
+    // turn inputs to value
+    let firstName = $('#in-firstName').val();
+    let lastName = $('#in-lastName').val();
+    let id = $('#in-id').val();
+    let title = $('in-title').val();
+    let annualSalary = $('#in-annualSalary').val();
+    // push to employeeArray
+    employeeArray.push( {
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        title: title,
+        annualSalary: annualSalary 
+    })// end object literal
+    // run math function
+
+    // run append function
+}// end function
+
+
+ // empty the inputs
+//  $('#in-firstName').empty();
+//  $('#in-lastName').empty();
+//  $('#in-id').empty();
+//  $('in-title').empty();
+//  $('#in-annualSalary').empty();
